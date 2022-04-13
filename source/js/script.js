7364e5c0b4c56143bf4ed71c63e1fb3240e589bd
Alena Karenina <karenina@corp.profi.travel>
2022-04-13 16:56:41 +0500

    доработка старта анимации

--- source/js/script.js
+++ source/js/script.js
@@ -7,8 +7,8 @@ import chat from './modules/chat.js';
 import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
-import FullPageScroll from './modules/full-page-scroll';
-import AccentTypographyBuild from './modules/animation-text';
+import FullPageScroll from './modules/full-page-scroll.js';
+import AccentTypographyBuild from './modules/animation-text.js';
 
 // init modules
 mobileHeight();
@@ -23,11 +23,28 @@ social();
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
 
-window.addEventListener('load', () => {
-  document.body.classList.add('loaded');
+window.addEventListener(`load`, () => {
+  document.body.classList.add(`loaded`);
 });
 
-const animationIntroTitle = new AccentTypographyBuild('.intro__title', 700, 'show-text', 'transform', false, 0);
-const animationIntroDate = new AccentTypographyBuild('.intro__date', 700, 'show-text', 'transform', true, 700);
-const animationHistoryTitle = new AccentTypographyBuild('.slider__item-title', 700, 'show-text', 'transform', true, 0);
-const animationPrizesTitle = new AccentTypographyBuild('.prizes__title', 700, 'show-text', 'transform', true, 1000);
+const animationIntroTitle = new AccentTypographyBuild(`.intro__title`, 700, `show-text`, `transform`, false, 0);
+const animationIntroDate = new AccentTypographyBuild(`.intro__date`, 700, `show-text`, `transform`, true, 700);
+const animationHistoryTitle = new AccentTypographyBuild(`.slider__item-title`, 700, `show-text`, `transform`, true, 0);
+const animationPrizesTitle = new AccentTypographyBuild(`.prizes__title`, 700, `show-text`, `transform`, true, 1000);
+
+function animationStarting() {
+  switch (document.location.hash) {
+    case `#prizes`:
+      const svg = document.getElementById(`primaryAnimation`);
+      const animation = document.getElementById(`airshipSceneFadeOutAnimation`);
+      if (svg && animation) {
+        animation.endElement();
+        svg.setCurrentTime(0);
+        animation.beginElement();
+      }
+      break;
+    default:
+      break;
+  }
+}
+animationStarting();
